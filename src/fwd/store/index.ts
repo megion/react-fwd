e56e568addf4052c00bab572bf39d3dbe85ea0d9
ilya <ilya@localhost.localdomain>
2021-03-08 19:22:03 +0300

    add react router

--- src/fwd/store/index.ts
+++ src/fwd/store/index.ts
@@ -1,19 +1,24 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createBrowserHistory } from 'history'
+import { createStore, applyMiddleware, Middleware } from 'redux'
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
 import rootReducer from './root-reducer'
+import { composeEnhancers } from './utils'
 
 // configure middlewares
-//export const history = createBrowserHistory();
+//export const history = createBrowserHistory()
 //const middlewares = [routerMiddleware(history), epicMiddleware]
+const middlewares: Middleware[] = [thunk, logger]
 
 // compose enhancers
-//const enhancer = composeEnhancers(applyMiddleware(...middlewares))
+const enhancer = composeEnhancers(applyMiddleware(...middlewares))
 
 // rehydrate state on app start
-//const initialState = {}
+const initialState = {}
 
 // create store
-//const store = createStore(rootReducer(), initialState, enhancer)
-const store = createStore(rootReducer())
+const store = createStore(rootReducer(), initialState, enhancer)
+//const store = createStore(rootReducer())
 
 // export store singleton instance
 export default store
